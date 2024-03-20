aeba12d8d3b6e3ee5d9ef740297079e34a70f67e
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-03-20 23:38:25 +0530

    removed bg red from nav

--- src/components/Navbaar.jsx
+++ src/components/Navbaar.jsx
@@ -2,10 +2,10 @@ import logo from "../assets/logo.ico"
 import menu from "../assets/menu.svg"
 function Navbaar(){
     return(
-        <div className=" bg-white fixed top-0 w-full z-10 py-2 px-2 flex justify-between items-center z-50">
+        <div className=" bg-white fixed top-0 w-full py-2 px-2 flex justify-between items-center z-50">
             <div className="flex">
                 <div><img className="w-12 h-8 pr-0" src={logo} alt="" /></div>
-                <div className="font-[800] text-[20px] font-TT Commons Pro sm:bg-red-600">humankind</div>
+                <div className="font-[800] text-[20px] font-TT Commons Pro">humankind</div>
             </div>
             <div className="hidden md:pr-6 md:flex md:gap-7 font-TT Commons Pro md:font-[700] md:text-[15px]">
                 <div className="flex justify-between items-center hover:text-gray-500 cursor-pointer">Wrestle</div>
