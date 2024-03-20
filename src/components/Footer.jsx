c0f9358e9249155eb790ce1d6257156b56913c38
kavyanshkumar18@gmail.com <kavyanshkumar18@gmail.com>
2024-03-21 03:21:06 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,11 +13,11 @@ function Footer() {
             <div className="text-center text-lg">stay human. be kind</div>
         </div>
         <div className="flex justify-center items-center gap-x-6 my-6">
-            <div className="w-8"><img src={d} alt="" /></div>
-            <div className="w-8"><img src={t} alt="" /></div>
-            <div className="w-8"><img src={i} alt="" /></div>
-            <div className="w-8"><img src={l} alt="" /></div>
-            <div className="w-8"><img src={o} alt="" /></div>
+            <div className="w-8"><a href="https://discord.com/invite/robotos" target="_blank"><img src={d} alt="" /></a></div>
+            <div className="w-8"><a href="https://twitter.com/humankindart" target="_blank"><img src={t} alt="" /></a></div>
+            <div className="w-8"><a href="https://www.instagram.com/humankind.art/" target="_blank"><img src={i} alt="" /></a></div>
+            <div className="w-8"><a href="https://www.linkedin.com/company/humankindart/" target="_blank"><img src={l} alt="" /></a></div>
+            <div className="w-8"><a href="https://opensea.io/collection/humankind-lunchboxes" target="_blank"><img src={o} alt="" /></a></div>
         </div>
         <div className="flex-col justify-center items-center ">
             <div className="md:flex md:justify-center md:items-center"><button className=" bg-black text-white rounded-3xl py-3 text-center cursor-pointer px-12 hover:bg-red-600 hover:border hover:border-black hover:border-b-4 hover:border-l-4 md:py-3 md:px-7 mb-5 mt-5 ml-12 md:ml-0 ">⚔️ Play Now!</button></div>
